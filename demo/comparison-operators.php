d65bdea2323e5c563726f8b5b21777397dddbbf6
manidf <mannuel.ferreira@gmail.com>
2017-04-05 22:59:24 +0200

    switch statements

---
+++ demo/comparison-operators.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>comparison operators</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+</head>
+<body>
+
+<?php
+if (4 === 4 && 5 < 10) {
+	echo "it is true";
+}
+?>
+
+<h2>Comparison Operators</h2>
+<pre>
+		equal =
+		identical ===
+		compare > <  >= <= <>
+		not equal !=
+		not identical !==
+	</pre>
+<h2>Logical Operators</h2>
+<pre>
+		And &&
+		Or ||
+		Not !
+	</pre>
+
+
+</body>
+</html>
