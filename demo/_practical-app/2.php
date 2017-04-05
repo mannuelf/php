f6be658d7d1f88abd3dc19962c8ea82a7091c90a
manidf <mannuel.ferreira@gmail.com>
2017-04-05 22:33:53 +0200

    exercise

--- demo/_practical-app/2.php
+++ demo/_practical-app/2.php
@@ -6,13 +6,13 @@
 		<aside class="col-xs-4">
 
 	<?php Navigation();?>
-			
-			
+
+
 		</aside><!--SIDEBAR-->
 
 
 		<article class="main-content col-xs-8">
-		
+
 
 
 		<?php
@@ -21,22 +21,37 @@
 
 		  Step 2: Add the two variables and display the sum with echo:
 
-
 		  Step3: Make 2 Arrays with the same values, one regular and the other associative
 
 		  Step4: Make a constant and set it to the value of PHP. and use an echo to print it out
+		*/
 
+		$number1 = 1;
+		$number2 = 10;
 
-			
+		echo $number1 + $number2;
 
-			 */
+		echo "<br/>";
 
-		
+		$numbers1 = array(1, 10);
 
+		$numbers = array(
+			"number1" => 1,
+			"number2" => 10,
+		);
+
+		echo $number1[0] . $numbers1[1];
+
+		echo "<br/>";
+
+		echo $numbers["number1"] . $numbers["number2"];
+
+		echo "<br/>";
+
+		define("PHP", "Is Pretty Awesome");
+		echo PHP;
 
 		?>
-
-	
 
 		</article><!--MAIN CONTENT-->
 
