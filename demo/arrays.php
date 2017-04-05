7f70ad9d3fabb557fab55365fa6437d56163c133
manidf <mannuel.ferreira@gmail.com>
2017-04-05 22:10:21 +0200

    arrays example

---
+++ demo/arrays.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Arrays</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+</head>
+<body>
+
+<?php
+
+	$numberList = array(23, 24, 25, '30', 26, 'hello', 'world', '<h2>End of array</h2>');
+
+	print_r($numberList);
+	$numberList[6];
+
+?>
+</body>
+</html>
