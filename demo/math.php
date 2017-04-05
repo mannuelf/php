970cee68500c0a736ce7c1def9cccd232ef8aa1f
manidf <mannuel.ferreira@gmail.com>
2017-04-05 22:03:28 +0200

    math examples

--- demo/math.php
+++ demo/math.php
@@ -5,22 +5,31 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Dynamic text</title>
+  <title>Math</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
 </head>
 <body>
 
 <?php
-	$name = "Mannuel Ferreira";
-	$number = 34;
+	echo 56 + 45;
+	echo "<br/>";
+	echo 56 - 45;
+	echo "<br/>";
+	echo 56 * 45;
+	echo "<br/>";
+	echo 56 / 45;
 
-	echo $name . ' is ' . $number . ' years old  ';
+	echo "<br/>";
 
-	$name = "<h1>Hello </h1>";
+	echo (56 + 56) - 45 * (56 / 45);
 
-	echo $name;
-	?>
+	echo "<br/>";
+	$number1 = 100;
+	$number2 = 200;
+
+	echo $number1 + $number2;
+?>
 
 </body>
 </html>
