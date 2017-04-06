5105e2c029fd5e6cf641094779ad42a681e3a72e
manidf <mannuel.ferreira@gmail.com>
2017-04-06 21:14:24 +0200

    practical looooops

--- demo/_practical-app/3.php
+++ demo/_practical-app/3.php
@@ -6,26 +6,63 @@
 	<aside class="col-xs-4">
 
 	<?php Navigation();?>
-			
+
 	</aside><!--SIDEBAR-->
 
 
 <article class="main-content col-xs-8">
 
-<?php  
+<?php
 
 /*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
 
-
-
 	Step 2: Make a forloop  that displays 10 numbers
 
-
 	Step 3 : Make a switch Statement that test againts one condition with 5 cases
-
  */
+	if(10 > 0) {
+		echo "I think php is great";
+	} elseif(0 < 10) {
+		echo "I love PHP";
+	} else {
+		echo "Maybe here";
+	}
 
-	
+	echo "<br/>";
+
+	for($counter = 0; $counter <= 10; $counter++) {
+		echo $counter . ', ';
+	}
+
+	echo "<br/>";
+
+	$number = 5;
+	switch($number) {
+		case 1:
+		echo "One";
+		break;
+
+		case 2:
+		echo "two";
+		break;
+
+		case 3:
+		echo "three";
+		break;
+
+		case 4:
+		echo "four";
+		break;
+
+		case 5:
+		echo "five";
+		break;
+
+		default :
+		echo "soz could not find anything";
+	}
+
+
 ?>
 
 
@@ -34,5 +71,5 @@
 
 
 </article><!--MAIN CONTENT-->
-	
+
 <?php include "includes/footer.php" ?>
