5948b4fde1233c39322b92757753ebc79862b62e
manidf <mannuel.ferreira@gmail.com>
2017-04-06 21:00:48 +0200

    foreach loop

---
+++ demo/for-each-loop.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>switch statements</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+</head>
+<body>
+
+<?php
+
+	$numbers = array(232,2323,545,7852,8763,2234);
+
+	foreach ($numbers as $number) {
+		echo $number . "<br/>";
+	}
+?>
+
+</body>
+</html>
