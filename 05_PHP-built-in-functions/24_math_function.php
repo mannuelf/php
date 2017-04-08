e1dfc1e372027b34f40e6cd2e8abbc5d035519eb
manidf <mannuel.ferreira@gmail.com>
2017-04-08 15:52:27 +0200

    math functions

---
+++ 05_PHP-built-in-functions/24_math_function.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Math Functions</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+</head>
+<body>
+
+<?php
+	echo pow(2,8);
+	echo "<br/>";
+
+	echo rand(1, 1000);
+	echo "<br/>";
+
+	echo sqrt(100);
+	echo "<br/>";
+
+	echo ceil(5.6);
+	echo "<br/>";
+
+	echo floor(3.6);
+	echo "<br/>";
+
+	echo round(3.6);
+	echo "<br/>";
+?>
+
+</body>
+</html>
