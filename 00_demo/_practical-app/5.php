1b1de0a5d2d25c9e5ae40cbcf413a2b5995dbcdc
manidf <mannuel.ferreira@gmail.com>
2017-04-08 16:22:06 +0200

    exercise five additions

--- 00_demo/_practical-app/5.php
+++ 00_demo/_practical-app/5.php
@@ -12,25 +12,38 @@
 <article class="main-content col-xs-8">
 
 	<?php
-	/*
-	 * Step1: Use a pre-built math function here and echo it
-	 * */
-	echo pow(3,4);
-	echo "<hr/>";
-	/*
-	 * Step 2:  Use a pre-built string function here and echo it
-	 * */
-	$newcastle = "Newcastle United";
-	echo strtoupper($newcastle);
-	echo "<hr/>";
-	/*
-	 * Step 3:  Use a pre-built Array function here and echo it
-	 * */
-	$numbers = [ 1982, 829, 928, 8272, 9272];
-	echo max($numbers);
-	echo "<hr/>";
-	sort($numbers);
-	print_r($numbers);
+		/*
+		 * Step1: Use a pre-built math function here and echo it
+		 * */
+		echo pow(3,4);
+		echo "<hr/>";
+		echo rand(1, 900);
+		echo "<hr/>";
+		/*
+		 * Step 2:  Use a pre-built string function here and echo it
+		 * */
+		$newcastle = "Newcastle United";
+		echo strtoupper($newcastle);
+		echo "<hr/>";
+		echo strlen($newcastle);
+		/*
+		 * Step 3:  Use a pre-built Array function here and echo it
+		 * */
+		$numbers = [ 1982, 829, 928, 8272, 9272];
+		echo max($numbers);
+		echo "<hr/>";
+
+		sort($numbers);
+		print_r($numbers);
+		echo "<hr/>";
+
+		$values = ['Alan Shearer', 2000, 'Ronaldo', 'Dunga', $newcastle];
+		$found = in_array($newcastle, $values);
+		if($found) {
+			echo 'We found ' . ' ' . $newcastle . ' in the array';
+		} else {
+			echo "we did not find it.";
+		}
 	?>
 
 </article><!--MAIN CONTENT-->
