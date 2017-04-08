25538f06fa85a6872c377e9d7a1e61fa96fedff2
manidf <mannuel.ferreira@gmail.com>
2017-04-08 13:44:31 +0200

    function scope, showing use of global variable

---
+++ 04_functions/functions-scope.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Functions scope</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+</head>
+<body>
+
+<h2>
+	<?php
+
+		$x = "outside"; // global
+
+		function convert() {
+			global $x;
+			$x = "inside"; // local
+		}
+
+		echo $x;
+		echo "<br/>";
+
+		convert();
+
+		echo $x;
+
+	?>
+</h2>
+</body>
+</html>
