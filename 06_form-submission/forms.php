0998119b34ab2c21b307ba26dc3252228695df29
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-04-09 20:34:46 +0200

    forms

--- 06_form-submission/forms.php
+++ 06_form-submission/forms.php
@@ -1,3 +1,8 @@
+<?php
+    if(isset($_POST['submit'])) {
+        echo "Yes it works";
+    }
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,30 +10,21 @@
 	<meta name="viewport"
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Math Functions</title>
+	<title>Forms</title>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
 </head>
 <body>
 
-<h2>
+<form action="forms.php" method="post">
+    <input type="text" placeholder="Enter Username" name="username">
+    <input type="password" placeholder="Enter Password" name="password">
+    <input type="submit" name="submit">
+</form>
+
 <?php
 
-	$string = "Hello whats up, hows it going";
-	echo strlen($string);
-	echo "<br/>";
-
-	echo strtoupper($string);
-	echo "<br/>";
-
-	echo strtolower($string);
-	echo "<br/>";
-
-	echo str_repeat($string, 4);
-	echo "<br/>";
-
-	echo str_shuffle($string);
 ?>
-</h2>
+
 </body>
 </html>
