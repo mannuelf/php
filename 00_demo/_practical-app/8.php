37ee6736885f17f5b5da1b08d8c6ffdb208205bf
manidf <mannuel.ferreira@gmail.com>
2017-04-25 22:59:33 +0200

    encrypt exercise

--- 00_demo/_practical-app/8.php
+++ 00_demo/_practical-app/8.php
@@ -3,35 +3,38 @@
 
 	<section class="content">
 
-		<aside class="col-xs-4">
+	<aside class="col-xs-4">
 
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+		<?php Navigation(); ?>
 
 
-		
+	</aside><!--SIDEBAR-->
+
+
 	<article class="main-content col-xs-8">
-	
-	
-	<?php  
-
-	/*  Step 1 -Make a variable with some text as value
-
-		Step 2 - Use crypt() function to encrypt it
-
-		Step 3 - Assign the crypt result to a variable
-
-		Step 4 - echo the variable
-
-	*/
-	
-	?>
 
 
+		<?php
+
+		/*  Step 1 -Make a variable with some text as value
+
+            Step 2 - Use crypt() function to encrypt it
+
+            Step 3 - Assign the crypt result to a variable
+
+            Step 4 - echo the variable
+
+        */
+		$myVariable = "osidhfiuewhbkjvnkdnj";
+		$hashFormat = "$2y$10$";
+		$salt = "987yhjkmcfd53twydgfvgt";
+		$hashFormatSalt = $hashFormat . $salt;
+
+		$password = crypt($myVariable, $hashFormatSalt);
+		echo $password;
+
+		?>
 
 
-
-</article><!--MAIN CONTENT-->
+	</article><!--MAIN CONTENT-->
 <?php include "includes/footer.php" ?>
