ce89e16c8a24e56980b045e9efb6f65147aeef75
manidf <mannuel.ferreira@gmail.com>
2017-06-26 06:50:17 +0200

    buld categories query and made widget

--- cms/includes/sidebar.php
+++ cms/includes/sidebar.php
@@ -18,18 +18,25 @@
 
 	<!-- Blog Categories Well -->
 	<div class="well">
+		<?php
+			$query = "SELECT * FROM cms.categories";
+
+			$select_all_categories = mysqli_query($dbConnection, $query);
+
+			if (!$select_all_categories) {
+				echo mysqli_error($select_all_categories);
+			}
+		?>
 		<h4>Blog Categories</h4>
 		<div class="row">
 			<div class="col-lg-6">
 				<ul class="list-unstyled">
-					<li><a href="#">Category Name</a>
-					</li>
-					<li><a href="#">Category Name</a>
-					</li>
-					<li><a href="#">Category Name</a>
-					</li>
-					<li><a href="#">Category Name</a>
-					</li>
+					<?php
+						while ($row = mysqli_fetch_assoc($select_all_categories)) {
+							$cat_title = $row['cat_title'];
+							echo "<li><a href='#'>{$cat_title}</a></li>";
+						}
+					?>
 				</ul>
 			</div>
 			<!-- /.col-lg-6 -->
@@ -51,11 +58,6 @@
 	</div>
 
 	<!-- Side Widget Well -->
-	<div class="well">
-		<h4>Side Widget Well</h4>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore,
-			perspiciatis adipisci accusamus laudantium odit aliquam repellat
-			tempore quos aspernatur vero.</p>
-	</div>
+  	<?php include "widget.php"; ?>
 
 </div>
