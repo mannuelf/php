dedcf784a92efcacee84b90fae7034d450e0d2a2
manidf <mannuel.ferreira@gmail.com>
2017-07-03 06:45:11 +0200

    categories form post

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -40,7 +40,7 @@
 							}
 						}
 					?>
-					<form action="categories.php" method="post" aria-labelledby="categories" class="form">
+					<form action="" method="post" aria-labelledby="categories" class="form">
 						<div class="form-group">
 							<label aria-label="cat_title" for="cat_title">Add Category</label>
 							<input type="text" name="cat_title" aria-labelledby="cat_title" title="cat_title" class="form-control">
