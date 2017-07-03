dbd1f80faab6135411991bef0a4b9b789bf207df
manidf <mannuel.ferreira@gmail.com>
2017-07-03 05:33:47 +0200

    create categories

--- cms/admin/index.php
+++ cms/admin/index.php
@@ -1,22 +1,16 @@
 <?php include "includes/header.php" ?>
 
-
-
 <div id="wrapper">
-
 	<!-- Navigation -->
 	<?php include "includes/navigation.php" ?>
-
 	<div id="page-wrapper">
-
 		<div class="container-fluid">
-
 			<!-- Page Heading -->
 			<div class="row">
 				<div class="col-lg-12">
 					<h1 class="page-header">
 						Welcome to CMS Admin
-						<small>Subheading</small>
+						<small>Author</small>
 					</h1>
 					<ol class="breadcrumb">
 						<li>
@@ -29,11 +23,8 @@
 				</div>
 			</div>
 			<!-- /.row -->
-
 		</div>
 		<!-- /.container-fluid -->
-
 	</div>
 	<!-- /#page-wrapper -->
 	<?php include "includes/footer.php" ?>
-
