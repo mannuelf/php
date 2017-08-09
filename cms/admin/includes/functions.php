382c5c09b0c3638e735d68421bb158cf46246e82
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-08-09 19:34:24 +0200

    created comfirm query function

--- cms/admin/includes/functions.php
+++ cms/admin/includes/functions.php
@@ -1,6 +1,18 @@
 <?php
 
 /**
+ * Error notice for db query
+ */
+function confirmQuery($result) {
+	global $dbConnection;
+
+	if ( ! $result) {
+		die("QUERY FAILED ." . mysqli_error($dbConnection));
+	}
+	return $result;
+}
+
+/**
  * Add a new category
  */
 function insert_categories() {
--- cms/admin/includes/posts_add-post.php
+++ cms/admin/includes/posts_add-post.php
@@ -1,5 +1,7 @@
 <?php
 	if(isset($_POST['add_post'])) {
+		global $dbConnection;
+		
 		date_default_timezone_set('UTC');
 		$post_title = $_POST['post_title'];
 		$post_author = $_POST['post_author'];
@@ -14,19 +16,32 @@
 		// move image to images folder
 		move_uploaded_file($post_image_temp, "../images/$post_image");
 
-		$query = "SELECT INTO posts(post_category_id,post_title,post_author,post_date,post_image,post_content,post_tags,post_comment_count,post_status";
+		$query = "INSERT INTO cms.posts(
+			post_category_id,
+			post_title,
+			post_author,
+			post_date,
+			post_image,
+			post_content,
+			post_tags,
+			post_comment_count,
+			post_status) ";
 		$query .= "VALUES(
-					 {$post_category_id},
-					'{$post_title}',
-					'{$post_author}',
-					now(),
-					'{$post_image}',
-					'{$post_content}',
-					'{$post_tags}',
-					'{$post_comment_count}',
-					'{$post_status}') ";
+			{$post_category_id},
+			'{$post_title}',
+			'{$post_author}',
+			now(),
+			'{$post_image}',
+			'{$post_content}',
+			'{$post_tags}',
+			'{$post_comment_count}',
+			'{$post_status}'); ";
 		
 		$create_post_query = mysqli_query($dbConnection, $query);
+
+		confirmQuery($create_post_query);
+
+	
 	}
 ?>
 
