03741ea9e3f3eb9d46f6e14f0dfd21f66270ccad
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-08-13 06:32:25 +0000

    Apply fixes from StyleCI

--- 00_demo/_practical-app/3.php
+++ 00_demo/_practical-app/3.php
@@ -1,11 +1,11 @@
-<?php include "functions.php" ?>
-<?php include "includes/header.php" ?>
+<?php include 'functions.php' ?>
+<?php include 'includes/header.php' ?>
 
 	<section class="content">
 
 	<aside class="col-xs-4">
 
-	<?php Navigation();?>
+	<?php Navigation(); ?>
 
 	</aside><!--SIDEBAR-->
 
@@ -16,52 +16,51 @@
 
 /*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
 
-	Step 2: Make a forloop  that displays 10 numbers
+    Step 2: Make a forloop  that displays 10 numbers
 
-	Step 3 : Make a switch Statement that test againts one condition with 5 cases
+    Step 3 : Make a switch Statement that test againts one condition with 5 cases
  */
-	if(10 > 0) {
-		echo "I think php is great";
-	} elseif(0 < 10) {
-		echo "I love PHP";
-	} else {
-		echo "Maybe here";
-	}
+    if (10 > 0) {
+        echo 'I think php is great';
+    } elseif (0 < 10) {
+        echo 'I love PHP';
+    } else {
+        echo 'Maybe here';
+    }
 
-	echo "<hr/>";
+    echo '<hr/>';
 
-	for($counter = 0; $counter <= 10; $counter++) {
-		echo $counter . ', ';
-	}
+    for ($counter = 0; $counter <= 10; $counter++) {
+        echo $counter.', ';
+    }
 
-	echo "<hr/>";
+    echo '<hr/>';
 
-	$number = 5;
-	switch($number) {
-		case 1:
-		echo "One";
-		break;
+    $number = 5;
+    switch ($number) {
+        case 1:
+        echo 'One';
+        break;
 
-		case 2:
-		echo "two";
-		break;
+        case 2:
+        echo 'two';
+        break;
 
-		case 3:
-		echo "three";
-		break;
+        case 3:
+        echo 'three';
+        break;
 
-		case 4:
-		echo "four";
-		break;
+        case 4:
+        echo 'four';
+        break;
 
-		case 5:
-		echo "five";
-		break;
+        case 5:
+        echo 'five';
+        break;
 
-		default :
-		echo "soz could not find anything";
-	}
-
+        default:
+        echo 'soz could not find anything';
+    }
 
 ?>
 
@@ -72,4 +71,4 @@
 
 </article><!--MAIN CONTENT-->
 
-<?php include "includes/footer.php" ?>
+<?php include 'includes/footer.php' ?>
--- 07_databases/mysql/login_create.php
+++ 07_databases/mysql/login_create.php
@@ -1,18 +1,18 @@
-<?php include "db.php"; ?>
-<?php include "functions.php"; ?>
-<?php include "./includes/header.php"; ?>
+<?php include 'db.php'; ?>
+<?php include 'functions.php'; ?>
+<?php include './includes/header.php'; ?>
 
 <div class="demo-layout-transparent mdl-layout mdl-js-layout">
 	<header class="mdl-layout__header mdl-layout__header--transparent">
 		<div class="mdl-layout__header-row">
 			<span class="mdl-layout-title">CREATE USER [".][",]</span>
 			<div class="mdl-layout-spacer"></div>
-            <?php require "./includes/navigation.php"; ?>
+            <?php require './includes/navigation.php'; ?>
 		</div>
 	</header>
     <div class="mdl-layout__drawer">
         <span class="mdl-layout-title">CREATE USER</span>
-        <?php require "./includes/navigation.php"; ?>
+        <?php require './includes/navigation.php'; ?>
     </div>
 	<main class="mdl-layout__content">
         <div class="mdl-grid"></div>
@@ -20,8 +20,8 @@
 			<div class="mdl-cell mdl-cell--8-col">
 				<h3>
 					<?php
-						createUser();
-					?>
+                        createUser();
+                    ?>
 				</h3>
 				<form action="login_create.php" method="post">
 					<div class="mdl-textfield mdl-js-textfield">
@@ -41,4 +41,4 @@
 	</main>
 </div>
 
-<?php include "./includes/footer.php"; ?>
+<?php include './includes/footer.php'; ?>
