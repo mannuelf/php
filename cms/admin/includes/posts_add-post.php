ff847b84e7b2db7f801cb8b0f42742776ea3f3fc
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-08-13 12:01:16 +0000

    Apply fixes from StyleCI

--- cms/admin/includes/posts_add-post.php
+++ cms/admin/includes/posts_add-post.php
@@ -1,22 +1,22 @@
 <?php
-	if(isset($_POST['add_post'])) {
-		global $dbConnection;
+    if (isset($_POST['add_post'])) {
+        global $dbConnection;
 
-		date_default_timezone_set('UTC');
-		$post_title = $_POST['post_title'];
-		$post_author = $_POST['post_author'];
-		$post_date = date('d-m-y');
-		$post_image = $_FILES['image']['name'];
-		$post_image_temp = $_FILES['image']['tmp_name'];
-		$post_content = $_POST['post_content'];
-		$post_tags = $_POST['post_tags'];
-		$post_comment_count = 4;
-		$post_status = $_POST['post_status'];
+        date_default_timezone_set('UTC');
+        $post_title = $_POST['post_title'];
+        $post_author = $_POST['post_author'];
+        $post_date = date('d-m-y');
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']['tmp_name'];
+        $post_content = $_POST['post_content'];
+        $post_tags = $_POST['post_tags'];
+        $post_comment_count = 4;
+        $post_status = $_POST['post_status'];
 
-		// move image to images folder
-		move_uploaded_file($post_image_temp, "../images/$post_image");
+        // move image to images folder
+        move_uploaded_file($post_image_temp, "../images/$post_image");
 
-		$query = "INSERT INTO cms.posts(
+        $query = 'INSERT INTO cms.posts(
 			post_title,
 			post_author,
 			post_date,
@@ -24,9 +24,9 @@
 			post_content,
 			post_tags,
 			post_comment_count,
-			post_status)";
+			post_status)';
 
-		$query .= "VALUES(
+        $query .= "VALUES(
 			'{$post_title}',
 			'{$post_author}', 
 			  now(),
@@ -36,8 +36,7 @@
 			'{$post_comment_count}',
 			'{$post_status}' ) ";
 
-		$create_post_query = mysqli_query($dbConnection, $query);
+        $create_post_query = mysqli_query($dbConnection, $query);
 
-		confirmQuery($create_post_query);
-	}
-?>
+        confirmQuery($create_post_query);
+    }
--- cms/admin/includes/posts_view-all-posts.php
+++ cms/admin/includes/posts_view-all-posts.php
@@ -1,6 +1,6 @@
 <?php
-	$query = "SELECT * FROM cms.posts";
-	$select_posts = mysqli_query($dbConnection, $query);
+    $query = 'SELECT * FROM cms.posts';
+    $select_posts = mysqli_query($dbConnection, $query);
 ?>
 <table class="table table-hover table-bordered">
 	<thead>
@@ -21,42 +21,42 @@
 	</thead>
 	<tbody>
 	<?php
-		// DELETE A POST
-		if(isset($_GET['delete'])){
-			global $dbConnection;
-			$the_post_id = $_GET['delete'];
-			$query = "DELETE FROM cms.posts WHERE id = {$the_post_id} ";
-			$delete_query = mysqli_query($dbConnection, $query);
-		}
-	?>
+        // DELETE A POST
+        if (isset($_GET['delete'])) {
+            global $dbConnection;
+            $the_post_id = $_GET['delete'];
+            $query = "DELETE FROM cms.posts WHERE id = {$the_post_id} ";
+            $delete_query = mysqli_query($dbConnection, $query);
+        }
+    ?>
 	<?php
-		while($row = mysqli_fetch_assoc($select_posts)) {
-			$post_id = $row['id'];
-			$cat_id = $row['post_category_id'];
-			$post_title = $row['post_title'];
-			$post_author = $row['post_author'];
-			$post_date = $row['post_date'];
-			$post_image = $row['post_image'];
-			$post_content = $row['post_content'];
-			$post_tags = $row['post_tags'];
-			$post_comment_count = $row['post_comment_count'];
-			$post_status = $row['post_status'];
-			echo "<tr>";
-			echo "<td>{$post_id}</td>";
-			echo "<td>{$cat_id}</td>";
-			echo "<td>{$post_title}</td>";
-			echo "<td>{$post_author}</td>";
-			echo "<td>{$post_date}</td>";
-			echo "<td><img src='../images/{$post_image}' width='100px'></td>";
-			echo "<td>{$post_content}</td>";
-			echo "<td>{$post_comment_count}</td>";
-			echo "<td>{$post_comment_count}</td>";
-			echo "<td>{$post_status}</td>";
-			echo "<td>{$post_date}</td>";
-			echo "<td><a href='./posts.php?source=edit_post&p_id={$post_id}'>edit</a></td>";
-			echo "<td><a href='./posts.php?delete={$post_id}'>delete</a></td>";
-			echo "</tr>";
-		}
-	?>
+        while ($row = mysqli_fetch_assoc($select_posts)) {
+            $post_id = $row['id'];
+            $cat_id = $row['post_category_id'];
+            $post_title = $row['post_title'];
+            $post_author = $row['post_author'];
+            $post_date = $row['post_date'];
+            $post_image = $row['post_image'];
+            $post_content = $row['post_content'];
+            $post_tags = $row['post_tags'];
+            $post_comment_count = $row['post_comment_count'];
+            $post_status = $row['post_status'];
+            echo '<tr>';
+            echo "<td>{$post_id}</td>";
+            echo "<td>{$cat_id}</td>";
+            echo "<td>{$post_title}</td>";
+            echo "<td>{$post_author}</td>";
+            echo "<td>{$post_date}</td>";
+            echo "<td><img src='../images/{$post_image}' width='100px'></td>";
+            echo "<td>{$post_content}</td>";
+            echo "<td>{$post_comment_count}</td>";
+            echo "<td>{$post_comment_count}</td>";
+            echo "<td>{$post_status}</td>";
+            echo "<td>{$post_date}</td>";
+            echo "<td><a href='./posts.php?source=edit_post&p_id={$post_id}'>edit</a></td>";
+            echo "<td><a href='./posts.php?delete={$post_id}'>delete</a></td>";
+            echo '</tr>';
+        }
+    ?>
 	</tbody>
 </table>
