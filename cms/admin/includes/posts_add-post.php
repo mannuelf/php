e437b788be74412eec6095fec402c94415146bdf
mannuelf <mannuel.ferreira@gmail.com>
2017-08-13 08:03:30 +0200

    remove the cat id was preventing from posting to database

--- cms/admin/includes/posts_add-post.php
+++ cms/admin/includes/posts_add-post.php
@@ -1,9 +1,8 @@
 <?php
 	if(isset($_POST['add_post'])) {
 		global $dbConnection;
-		
+
 		date_default_timezone_set('UTC');
-		$post_category_id = $_POST['post_category_id'];
 		$post_title = $_POST['post_title'];
 		$post_author = $_POST['post_author'];
 		$post_date = date('d-m-y');
@@ -15,21 +14,19 @@
 		$post_status = $_POST['post_status'];
 
 		// move image to images folder
-		move_uploaded_file($post_image_temp, "../images/$post_image");
+		move_uploaded_file($post_image_temp, "../images/{$post_image}");
 
 		$query = "INSERT INTO cms.posts(
-			post_category_id,
-			post_title, 
-			post_author, 
-			post_date, 
-			post_image, 
-			post_content, 
-			post_tags, 
-			post_comment_count, 
-			post_status) ";
-		
+			post_title,
+			post_author,
+			post_date,
+			post_image,
+			post_content,
+			post_tags,
+			post_comment_count,
+			post_status)";
+
 		$query .= "VALUES(
-			 {$post_category_id},
 			'{$post_title}',
 			'{$post_author}', 
 			  now(),
@@ -38,9 +35,7 @@
 			'{$post_tags}',
 			'{$post_comment_count}',
 			'{$post_status}' ) ";
-		
-		var_dump($query);
-		
+
 		$create_post_query = mysqli_query($dbConnection, $query);
 
 		confirmQuery($create_post_query);
@@ -59,20 +54,9 @@
 		<input name="post_author" type="text" class="form-control">
 	</div>
 
-	 <div class="form-group">
-		<label for="post_date">Post Date</label>
-		<input name="post_date" type="date" class="form-control">
-	</div> 
-
 	<div class="form-group">
 		<label for="post_image">Post Image</label>
 		<input name="post_image" type="file" class="form-control">
-	</div>
-
-	<div class="form-group">
-		<label for="post_content">Post Content</label>
-		<textarea name="post_content" id="" cols="30" rows="10" 
-		type="text" class="form-control"></textarea>
 	</div>
 
 	<div class="form-group">
@@ -88,6 +72,11 @@
 	<div class="form-group">
 		<label for="post_status">Post Status</label>
 		<input name="post_status" type="text" class="form-control">
+	</div>
+
+	<div class="form-group">
+		<label for="post_content">Post Content</label>
+		<textarea name="post_content" id="" cols="30" rows="10" type="text" class="form-control"></textarea>
 	</div>
 
 	<div class="form-group">
