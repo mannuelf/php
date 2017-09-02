9e5c1a1017ba809e898e436cebd103ca4912a7f5
mannuelf <mannuel.ferreira@gmail.com>
2017-09-02 20:09:48 +0200

    build relation to post with post category ID

--- cms/admin/includes/posts_view-all-posts.php
+++ cms/admin/includes/posts_view-all-posts.php
@@ -25,15 +25,15 @@
 		if(isset($_GET['delete'])){
 			global $dbConnection;
 			$the_post_id = $_GET['delete'];
-			$query = "DELETE FROM cms.posts WHERE id = {$the_post_id} ";
+			$query = "DELETE FROM cms.posts WHERE id = {$the_post_id}";
 			$delete_query = mysqli_query($dbConnection, $query);
 		}
 	?>
 	<?php
 		while($row = mysqli_fetch_assoc($select_posts)) {
 			$post_id = $row['id'];
-			$cat_id = $row['post_category_id'];
 			$post_title = $row['post_title'];
+			$post_category_id = $row['post_category_id'];
 			$post_author = $row['post_author'];
 			$post_date = $row['post_date'];
 			$post_image = $row['post_image'];
@@ -43,9 +43,18 @@
 			$post_status = $row['post_status'];
 			echo "<tr>";
 			echo "<td>{$post_id}</td>";
-			echo "<td>{$cat_id}</td>";
-			echo "<td>{$post_title}</td>";
+
+			// fetch the category title from the DB to
+//			$query =  "SELECT * FROM cms.categories WHERE $post_id = {$post_category_id}";
+//			$select_categories_id = mysqli_query($dbConnection, $query);
+//			while($row = mysqli_fetch_assoc($select_categories_id)) {
+//				$cat_id = $row['cat_id'];
+//				$cat_title = $row['cat_title'];
+//				echo "<td>{$cat_title}</td>";
+//			}
+			echo "<td>{$post_category_id}</td>";
 			echo "<td>{$post_author}</td>";
+			echo "<td>{$post_title}</td>";
 			echo "<td>{$post_date}</td>";
 			echo "<td><img src='../images/{$post_image}' width='100px'></td>";
 			echo "<td>{$post_content}</td>";
