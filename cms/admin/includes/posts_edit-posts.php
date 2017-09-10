e0c05fc4ba3491b7aef87f48d0eba0dccf1b4adc
mannuelf <mannuel.ferreira@gmail.com>
2017-09-10 18:18:37 +0200

    hide the image not working

--- cms/admin/includes/posts_edit-posts.php
+++ cms/admin/includes/posts_edit-posts.php
@@ -14,11 +14,11 @@ confirmQuery($select_posts_by_id);
 
 while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 	$post_id = $row['id'];
-	$cat_id = $row['post_category_id'];
+	$post_cat_id = $row['post_category_id'];
 	$post_title = $row['post_title'];
 	$post_author = $row['post_author'];
 	$post_date = $row['post_date'];
-	$post_image = $row['post_image'];
+//	$post_image = $row['post_image'];
 	$post_content = $row['post_content'];
 	$post_tags = $row['post_tags'];
 	$post_comment_count = $row['post_comment_count'];
@@ -26,30 +26,30 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 }
 
 if(isset($_POST['edit_post'])) {
-	$cat_id = $_POST['post_category_id'];
 	$post_title = $_POST['post_title'];
+	$post_category_id = $_POST['post_category_id'];
 	$post_author = $_POST['post_author'];
 	$post_date = $_POST['post_date'];
+//	$post_image = $_FILES['image']['name'];
+//	$post_image_temp = $_FILES['image']['tmp_name'];
 	$post_content = $row['post_content'];
-	$post_image = $_FILES['image']['name'];
-	$post_image_temp = $_FILES['image']['tmp_name'];
 	$post_tags = $_POST['post_tags'];
 	$post_comment_count = $_POST['post_comment_count'];
 	$post_status = $_POST['post_status'];
 
-	move_uploaded_file($post_image_temp, "../images/$post_image");
+//	move_uploaded_file($post_image_temp, "../images/$post_image");
 
 	$query = "UPDATE cms.posts SET ";
-	$query .= "post_title = '{$post_title}', ";
-	$query .= "post_category_id = '{$cat_id}', ";
-	$query .= "post_author = '{$post_author}', ";
-	$query .= "post_date = now(), ";
-	$query .= "post_content = '{$post_content}', ";
-	$query .= "post_image = '{$post_image}', ";
-	$query .= "post_tags = '{$post_tags}', ";
-	$query .= "post_comment_count= '{$post_comment_count}', ";
-	$query .= "post_status = '{$post_status}' ";
-	$query .= "WHERE post_id = {$cat_id}";
+		$query .= "post_title = '{$post_title}', ";
+		$query .= "post_category_id = '{$post_category_id}', ";
+		$query .= "post_author = '{$post_author}', ";
+		$query .= "post_date = now(), ";
+		$query .= "post_content = '{$post_content}', ";
+//		$query .= "post_image = '{$post_image}', ";
+		$query .= "post_tags = '{$post_tags}', ";
+		$query .= "post_comment_count= '{$post_comment_count}', ";
+		$query .= "post_status = '{$post_status}' ";
+	$query .= "WHERE cms.posts.post_category_id = {$the_post_id} ";
 
 	$edit_post = mysqli_query($dbConnection, $query);
 
@@ -74,9 +74,9 @@ if(isset($_POST['edit_post'])) {
 				confirmQuery($select_categories_id);
 
 				while($row = mysqli_fetch_assoc($select_categories_id)) {
-					$cat_id = $row['cat_id'];
+					$post_cat_id = $row['cat_id'];
 					$cat_title = $row['cat_title'];
-					echo "<option value='{$cat_id}'>{$cat_title}</option>";
+					echo "<option value='{$post_cat_id}'>{$cat_title}</option>";
 				}
 			?>
 		</select>
@@ -85,11 +85,11 @@ if(isset($_POST['edit_post'])) {
 		<label for="post_author">Post Author</label>
 		<input value="<?php echo $post_author; ?>" name="post_author" type="text" class="form-control">
 	</div>
-	<div class="form-group">
-		<img src="../images/<?php echo $post_image; ?>" width="200" alt=""> <br>
-		<label for="post_image">Post Image</label>
-		<input name="post_image" type="file" class="form-control">
-	</div>
+<!--	<div class="form-group">-->
+<!--		<img src="../images/--><?php //echo $post_image; ?><!--" width="200" alt=""> <br>-->
+<!--		<label for="post_image">Post Image</label>-->
+<!--		<input name="post_image" type="file" class="form-control">-->
+<!--	</div>-->
 	<div class="form-group">
 		<label for="post_tags">Post Tags</label>
 		<input value="<?php echo $post_tags; ?>" name="post_tags" type="text" class="form-control">
