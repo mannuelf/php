ac0930573b747bcd642a947a6949c7bcc9c1fc3c
mannuelf <mannuel.ferreira@gmail.com>
2017-09-16 08:51:48 +0200

    update post page now posting with an image

--- cms/admin/includes/posts_add-post.php
+++ cms/admin/includes/posts_add-post.php
@@ -28,13 +28,13 @@
 
 		$query .= "VALUES(
 			'{$post_title}',
-			'{$post_author}', 
-			'{$post_image}',
+			'{$post_author}',
 			  now(),
+			'{$post_image}',
 			'{$post_content}',
 			'{$post_tags}',
-			'{$post_comment_count}',
-			'{$post_status}' ) ";
+			'{$post_status}',
+			'{$post_comment_count}') ";
 
 		$create_post_query = mysqli_query($dbConnection, $query);
 
