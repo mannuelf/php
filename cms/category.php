920733ac55c35bd6fc12be446effa4e6f1bd4543
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2017-09-20 20:46:19 +0000

    Apply fixes from StyleCI

--- cms/category.php
+++ cms/category.php
@@ -1,6 +1,6 @@
-<?php include "database/db.php" ?>
-<?php include "includes/header.php" ?>
-<?php include "includes/navigation.php" ?>
+<?php include 'database/db.php' ?>
+<?php include 'includes/header.php' ?>
+<?php include 'includes/navigation.php' ?>
 <!-- Page Content -->
 <div class="container">
 	<div class="row">
@@ -13,32 +13,30 @@
 
 			<?php
 
-				if (isset($_GET['category'])) {
-					$post_category_id = $_GET['category'];
+                if (isset($_GET['category'])) {
+                    $post_category_id = $_GET['category'];
+                }
+                $query = "SELECT * FROM cms.posts WHERE cms.posts.post_category_id = $post_category_id";
 
-				}
-				$query = "SELECT * FROM cms.posts WHERE cms.posts.post_category_id = $post_category_id";
+                $select_all_posts = mysqli_query($dbConnection, $query);
 
-				$select_all_posts = mysqli_query($dbConnection, $query);
+                if (!$select_all_posts) {
+                    echo mysqli_error($select_all_posts);
+                }
 
-				if ( !$select_all_posts) {
-					echo mysqli_error($select_all_posts);
-				}
+                if (isset($_GET['p_id'])) {
+                    $post_id = $_GET['p_id'];
+                }
 
-				if(isset($_GET['p_id'])) {
-					$post_id = $_GET['p_id'];
-				}
-
-				while ($row = mysqli_fetch_assoc($select_all_posts)) {
-					$post_id = $row['id'];
-					$post_title = $row['post_title'];
-					$post_author = $row['post_author'];
-					$post_date = $row['post_date'];
-					$post_image = $row['post_image'];
-					$post_content = $row['post_content'];
-					$post_tags = $row['post_tags'];
-					// break out of the while loop (meh looks dodgy but it works eh)
-					?>
+                while ($row = mysqli_fetch_assoc($select_all_posts)) {
+                    $post_id = $row['id'];
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+                    $post_tags = $row['post_tags'];
+                    // break out of the while loop (meh looks dodgy but it works eh) ?>
 					<h2>
 						<!--
 							Pass the url a parameter with the key of the array of the GET super global for the id's
@@ -64,7 +62,8 @@
 							class="glyphicon glyphicon-chevron-right"></span></a>
 					<hr>
 
-				<?php } ?>
+				<?php
+                } ?>
 
 
 			<!-- Pager -->
@@ -77,7 +76,7 @@
 				</li>
 			</ul>
 		</div>
-		<?php include "includes/sidebar.php" ?>
+		<?php include 'includes/sidebar.php' ?>
 	</div>
 	<!-- /.row -->
-	<?php include "includes/footer.php" ?>
+	<?php include 'includes/footer.php' ?>
--- cms/database/db.php
+++ cms/database/db.php
@@ -1,22 +1,16 @@
 
 <?php
-	$db['db_host'] = 'localhost';
-	$db['db_user'] = 'root';
-	$db['db_pass'] = 'root';
-	$db['db_name'] = 'cms';
+    $db['db_host'] = 'localhost';
+    $db['db_user'] = 'root';
+    $db['db_pass'] = 'root';
+    $db['db_name'] = 'cms';
 
-	foreach ($db as $key => $value) {
-		define(strtoupper($key), $value);
-	}
+    foreach ($db as $key => $value) {
+        define(strtoupper($key), $value);
+    }
 
-	$dbConnection = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
+    $dbConnection = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
 
-	if (! $dbConnection) {
-		die('Connection Error: '. mysqli_connect_error());
-	}
-
-
-
-
-
-
+    if (!$dbConnection) {
+        die('Connection Error: '.mysqli_connect_error());
+    }
