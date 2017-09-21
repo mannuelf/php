037e5d1b630293bf3614c8643fa8df2feb68b108
mannuelf <mannuel.ferreira@gmail.com>
2017-09-21 14:38:53 +0200

    build comments page and area

---
+++ cms/admin/comments.php
@@ -0,0 +1,52 @@
+<?php include "includes/admin_header.php" ?>
+
+<div id="wrapper" class="categories">
+	<!-- Navigation -->
+	<?php include "includes/admin_navigation.php" ?>
+	<div id="page-wrapper">
+		<div class="container-fluid">
+			<!-- Page Heading -->
+			<div class="row">
+				<div class="col-lg-12">
+					<h1 class="page-header">
+						Comments
+						<small>- All the comments from the posts</small>
+					</h1>
+					<ol class="breadcrumb">
+						<li>
+							<i class="fa fa-dashboard"></i> <a href="index.php">Dashboard</a>
+						</li>
+						<li class="active">
+							<i class="fa fa-file"></i> Posts
+						</li>
+					</ol>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+				<?php
+					if(isset($_GET['source'])) {
+						$source = $_GET['source'];
+					} else {
+						$source = "";
+					}
+					switch($source) {
+						case "add_post":
+							include "./includes/posts_add-post.php";
+							break;
+						case "edit_post":
+							include "./includes/posts_edit-posts.php";
+							break;
+						default;
+							include "./includes/posts_view-all-comments.php";
+							break;
+					}
+				?>
+				</div>
+			</div>
+			<!-- /.row -->
+		</div>
+		<!-- /.container-fluid -->
+	</div>
+	<!-- /#page-wrapper -->
+	<?php include "includes/admin_footer.php" ?>
