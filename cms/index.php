631ef822594c24ded82b3d59333397087f953c46
mannuelf <mannuel.ferreira@gmail.com>
2017-09-26 22:55:12 +0200

    fixed links to post

--- cms/index.php
+++ cms/index.php
@@ -54,7 +54,7 @@
 					 class="img-responsive" alt="<?php echo $post_title ?>">
 				<hr>
 				<p><?php echo $post_content ?></p>
-				<a class="btn btn-primary" href="#">Read More <span
+				<a class="btn btn-primary" href="./post.php?p_id=<?php echo $post_id ?>">Read More <span
 						class="glyphicon glyphicon-chevron-right"></span></a>
 				<hr>
 
