643b22e2ee03df3deb46fe46505402480ce75ae8
mannuelf <mannuel.ferreira@gmail.com>
2017-10-01 22:07:43 +0200

    delete comment function inplace

--- cms/admin/includes/posts_view-all-comments.php
+++ cms/admin/includes/posts_view-all-comments.php
@@ -16,51 +16,50 @@
 		<th><abbr title="Post Title That is related to this comment">Post Title</abbr></th>
 		<th>Date</th>
 		<th>Approve</th>
-		<th>UnApprove</th>
-		<th>Edit</th>
+		<th>Unapproved</th>
 		<th>Delete</th>
 	</tr>
 	</thead>
 	<tbody>
 	<?php
-		// DELETE A POST
+		// DELETE A COMMENT
 		if(isset($_GET['delete'])){
 			global $dbConnection;
-			$the_post_id = $_GET['delete'];
-			$query = "DELETE FROM cms.posts WHERE id = {$the_post_id}";
+			$the_comment_id = $_GET['delete'];
+			$query = "DELETE FROM cms.comments WHERE cms.comments.comment_id = {$the_comment_id}";
 			$delete_query = mysqli_query($dbConnection, $query);
+			header("Location: comments.php");
 		}
-		?>
-		<?php
-			while($row = mysqli_fetch_assoc($select_posts)) {
-				$comment_id = $row['comment_id'];
-				$comment_post_id = $row['comment_post_id'];
-				$comment_author = $row['comment_author'];
-				$comment_content = $row['comment_content'];
-				$comment_email = $row['comment_email'];
-				$comment_status = $row['comment_status'];
-				$comment_date = $row['comment_date'];
+	?>
+	<?php
+		while($row = mysqli_fetch_assoc($select_posts)) {
+			$comment_id = $row['comment_id'];
+			$comment_post_id = $row['comment_post_id'];
+			$comment_author = $row['comment_author'];
+			$comment_content = $row['comment_content'];
+			$comment_email = $row['comment_email'];
+			$comment_status = $row['comment_status'];
+			$comment_date = $row['comment_date'];
 
-				echo "<tr>";
-				echo "<td>{$comment_id}</td>";
-				echo "<td>{$comment_post_id}</td>";
-				echo "<td>{$comment_author}</td>";
-				echo "<td>{$comment_content}</td>";
-				echo "<td>{$comment_email}</td>";
-				echo "<td>{$comment_status}</td>";
-				$query = "SELECT * FROM cms.posts WHERE cms.posts.id = $comment_post_id";
-				$select_posts_id_query = mysqli_query($dbConnection, $query);
-				while ($row = mysqli_fetch_assoc($select_posts_id_query)) {
-					$post_id = $row['id'];
-					$post_title = $row['post_title'];
-					echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
-				}
-				echo "<td>{$comment_date}</td>";
-				echo "<td><a href='./posts.php?source=edit_post&p_id=	'>Approve</a></td>";
-				echo "<td><a href='./posts.php?delete=	'>UnApprove</a></td>";
-				echo "<td><a href='./posts.php?source=edit_post&p_id=	'>Edit</a></td>";
-				echo "<td><a href='./posts.php?delete=	'>Delete</a></td>";
-				echo "</tr>";
+			echo "<tr>";
+			echo "<td>{$comment_id}</td>";
+			echo "<td>{$comment_post_id}</td>";
+			echo "<td>{$comment_author}</td>";
+			echo "<td>{$comment_content}</td>";
+			echo "<td>{$comment_email}</td>";
+			echo "<td>{$comment_status}</td>";
+			$query = "SELECT * FROM cms.posts WHERE cms.posts.id = $comment_post_id";
+			$select_posts_id_query = mysqli_query($dbConnection, $query);
+			while ($row = mysqli_fetch_assoc($select_posts_id_query)) {
+				$post_id = $row['id'];
+				$post_title = $row['post_title'];
+				echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
+			}
+			echo "<td>{$comment_date}</td>";
+			echo "<td><a href='./posts.php?source=edit_post&p_id=	'>Approve</a></td>";
+			echo "<td><a href='./posts.php?delete=	'>UnApprove</a></td>";
+			echo "<td><a href='./comments.php?delete=$comment_id'>Delete</a></td>";
+			echo "</tr>";
 		}
 	?>
 	</tbody>
