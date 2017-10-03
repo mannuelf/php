ddec61e6ddd0e4371ee4c90a24850ba59fde2cf5
mannuelf <mannuel.ferreira@gmail.com>
2017-10-03 22:06:11 +0200

    building up comments box

--- cms/post.php
+++ cms/post.php
@@ -93,17 +93,33 @@
 			<hr>
 
 			<!-- Comment -->
-			<div class="media">
-				<a class="pull-left" href="#">
-					<img class="media-object" src="http://placehold.it/64x64" alt="">
-				</a>
-				<div class="media-body">
-					<h4 class="media-heading">Start Bootstrap
-						<small>August 25, 2014 at 9:30 PM</small>
-					</h4>
-					Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-				</div>
-			</div>
+			<?php
+				$show_comment_query = "SELECT * FROM cms.comments WHERE cms.comments.comment_post_id = {$the_post_id} ";
+				$show_comment_query .= "AND cms.comments.comment_status = 'Approved' ";
+				$show_comment_query .= "ORDER BY cms.comments.comment_id DESC ";
+
+				$select_comment_query = mysqli_query($dbConnection, $show_comment_query);
+				confirmQuery($select_comment_query);
+
+				while ($row = mysqli_fetch_array($select_comment_query)) {
+					$comment_date = $row['comment_date'];
+					$comment_content = $row['comment_content'];
+					$comment_author = $row['comment_author'];
+				?>
+					<div class="media">
+						<a class="pull-left" href="#">
+							<img class="media-object" src="http://placehold.it/64x64" alt="">
+						</a>
+						<div class="media-body">
+							<h4 class="media-heading">
+								<?php echo $comment_author; ?>
+								<small><?php echo $comment_date; ?></small>
+							</h4>
+							<?php echo $comment_content; ?>
+						</div>
+					</div>
+			<?php } ?>
+
 		</div>
 		<?php include "includes/sidebar.php" ?>
 	</div>
