d85dfcc14d1083378a4bc1bcb4479925b99374b0
mannuelf <mannuel.ferreira@gmail.com>
2017-10-12 23:55:56 +0200

    building up login query

--- cms/includes/login.php
+++ cms/includes/login.php
@@ -2,8 +2,15 @@
 
 <?php
 	if (isset($_POST['login'])) {
-		echo "found";
-		echo $username = $_POST['username'];
-		echo $password = $_POST['password'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$username = mysqli_real_escape_string($dbConnection, $username);
+		$password = mysqli_real_escape_string($dbConnection, $password);
+
+		$query = "SELECT * FROM cms.users WHERE username = '{ $username }'";
+		$select_user_query = mysqli_query($dbConnection, $query);
+		if ( ! $select_user_query) {
+			die("QUERY FAILED" . mysqli_error($dbConnection));
+		}
 	}
 ?>
