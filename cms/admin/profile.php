d4f28944bf8e053f9878f5dd4991f0557d80cab3
mannuelf <mannuel.ferreira@gmail.com>
2017-10-19 07:36:12 +0200

    update user profile

--- cms/admin/profile.php
+++ cms/admin/profile.php
@@ -1,4 +1,54 @@
 <?php include "includes/admin_header.php" ?>
+<?php
+global $dbConnection;
+
+if(isset($_GET['edit_user'])) {
+	$the_user_id = $_GET['edit_user'];
+
+	$query = "SELECT * FROM cms.users";
+	$select_users_query = mysqli_query($dbConnection, $query);
+	while($row = mysqli_fetch_assoc($select_users_query)) {
+		$user_id = $row['id'];
+		$user_name = $row['user_name'];
+		$user_password = $row['user_password'];
+		$user_firstname = $row['user_firstname'];
+		$user_secondname = $row['user_secondname'];
+		$user_email = $row['user_email'];
+		$user_role = $row['user_role'];
+		$user_image = $row['user_image'];
+	}
+}
+
+if(isset($_POST['edit_user'])) {
+	$the_user_id = $_GET['edit_user'];
+	$user_name = $_POST['user_name'];
+	$user_password = $_POST['user_password'];
+	$user_firstname = $_POST['user_firstname'];
+	$user_secondname = $_POST['user_secondname'];
+	$user_email = $_POST['user_email'];
+	$user_role = $_POST['user_role'];
+
+	$user_image = $_FILES['user_image']['name'];
+	$user_image_temp = $_FILES['user_image']['tmp_name'];
+
+	// move image to images folder
+	move_uploaded_file($user_image_temp, "../images/$user_image");
+
+	$query = "UPDATE cms.users SET ";
+	$query .= "user_name = '{$user_name}', ";
+	$query .= "user_password = '{$user_password}', ";
+	$query .= "user_firstname = '{$user_firstname}', ";
+	$query .= "user_secondname = '{$user_secondname}', ";
+	$query .= "user_email = '{$user_email}', ";
+	$query .= "user_role = '{$user_role}', ";
+	$query .= "user_image = '{$user_image}' ";
+	$query .= "WHERE cms.users.user_name = {$username}";
+
+	$edit_user_query = mysqli_query($dbConnection, $query);
+
+	confirmQuery($edit_user_query);
+}
+?>
 <div id="wrapper" class="categories">
 	<!-- Navigation -->
 	<?php include "includes/admin_navigation.php" ?>
@@ -95,7 +145,7 @@
 						</div>
 
 						<div class="form-group">
-							<input type="submit" name="update_profile" aria-labelledby="update_profile" value="Update Profile" class="btn btn-primary">
+							<input type="submit" name="update_profile	" aria-labelledby="update_profile" value="Update Profile" class="btn btn-primary">
 						</div>
 					</form>
 
