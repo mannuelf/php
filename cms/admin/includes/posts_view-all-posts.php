732f8afe7fb47d883e9aacf099d43208cbabee7e
mannuelf <mannuel.ferreira@gmail.com>
2017-11-08 22:24:22 +0200

    add form and check box to table for bulk actions

--- cms/admin/includes/posts_view-all-posts.php
+++ cms/admin/includes/posts_view-all-posts.php
@@ -1,49 +1,69 @@
 <?php
-	$query = "SELECT * FROM cms.posts";
-	$select_posts = mysqli_query($dbConnection, $query);
+$query = "SELECT * FROM cms.posts";
+$select_posts = mysqli_query($dbConnection, $query);
 
-	confirmQuery($select_posts);
+confirmQuery($select_posts);
 ?>
-<table class="table table-hover table-bordered">
-	<thead>
-	<tr>
-		<th>Id</th>
-		<th>Author</th>
-		<th>Title</th>
-		<th>Category</th>
-		<th>Status</th>
-		<th>Image</th>
-		<th>Content</th>
-		<th>Tags</th>
-		<th>Comments</th>
-		<th>Date</th>
-		<th>Edit</th>
-		<th>Delete</th>
-	</tr>
-	</thead>
-	<tbody>
-	<?php
-		while($row = mysqli_fetch_assoc($select_posts)) {
-			$post_id = $row['id'];
-			$post_title = $row['post_title'];
-			$post_category_id = $row['post_category_id'];
-			$post_author = $row['post_author'];
-			$post_date = $row['post_date'];
-			$post_image = $row['post_image'];
-			$post_content = $row['post_content'];
-			$post_tags = $row['post_tags'];
-			$post_comment_count = $row['post_comment_count'];
-			$post_status = $row['post_status'];
+<form action="" method="post">
+	<div class="row col-lg-12 col-md-12">
+		<div class="bulk-options-container col-xs-4">
+			<select name="" id="" class="form-control">
+				<option value="">Select options</option>
+				<option value="">Publish</option>
+				<option value="">Draft</option>
+				<option value="">Delete</option>
+			</select>
+		</div>
+		<div class="col-xs-3">
+			<input type="submit" class="btn btn-default">
+			<a href="posts_add-post.php"  class="btn btn-info">Add New</a>
+		</div>
+	</div>
+	<div class="row col-lg-12 col-md-12">
+		<table class="table table-hover table-bordered">
+			<thead>
+			<tr>
+				<th><input id="jqSelectAllBoxes" type="checkbox"></th>
+				<th>Id</th>
+				<th>Author</th>
+				<th>Title</th>
+				<th>Category</th>
+				<th>Status</th>
+				<th>Image</th>
+				<th>Content</th>
+				<th>Tags</th>
+				<th>Comments</th>
+				<th>Date</th>
+				<th>Edit</th>
+				<th>Delete</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php
+			while ($row = mysqli_fetch_assoc($select_posts)) {
+				$post_id = $row['id'];
+				$post_title = $row['post_title'];
+				$post_category_id = $row['post_category_id'];
+				$post_author = $row['post_author'];
+				$post_date = $row['post_date'];
+				$post_image = $row['post_image'];
+				$post_content = $row['post_content'];
+				$post_tags = $row['post_tags'];
+				$post_comment_count = $row['post_comment_count'];
+				$post_status = $row['post_status'];
 
-			echo "<tr>";
+				echo "<tr>";
+				?>
+					<td><input class='jqCheckBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id; ?>'></td>
+				<?php
 				echo "<td>{$post_id}</td>";
 				echo "<td>{$post_author}</td>";
 				echo "<td>{$post_title}</td>";
 
 				// fetch the category title from the DB
-				$query =  "SELECT * FROM cms.categories WHERE cms.categories.cat_id = {$post_category_id}";
+				$query = "SELECT * FROM cms.categories WHERE cms.categories.cat_id = {$post_category_id}";
 				$select_categories_id = mysqli_query($dbConnection, $query);
-				while($row = mysqli_fetch_assoc($select_categories_id)) {
+				while ($row = mysqli_fetch_assoc($select_categories_id)) {
 					$cat_id = $row['cat_id'];
 					$cat_title = $row['cat_title'];
 					echo "<td>{$cat_title}</td>";
@@ -56,18 +76,21 @@
 				echo "<td>{$post_date}</td>";
 				echo "<td><a href='./posts.php?source=edit_post&p_id={$post_id}'>edit</a></td>";
 				echo "<td><a href='./posts.php?delete={$post_id}'>delete</a></td>";
-			echo "</tr>";
-		}
-	?>
-	<?php
-		// DELETE A POST
-		if(isset($_GET['delete'])){
-			global $dbConnection;
-			$the_post_id = $_GET['delete'];
-			$query = "DELETE FROM cms.posts WHERE id = {$the_post_id}";
-			$delete_query = mysqli_query($dbConnection, $query);
-			confirmQuery($delete_query);
-		}
-	?>
-	</tbody>
-</table>
+				echo "</tr>";
+			}
+			?>
+			<?php
+			// DELETE A POST
+			if (isset($_GET['delete'])) {
+				global $dbConnection;
+				$the_post_id = $_GET['delete'];
+				$query = "DELETE FROM cms.posts WHERE id = {$the_post_id}";
+				$delete_query = mysqli_query($dbConnection, $query);
+				confirmQuery($delete_query);
+			}
+			?>
+			</tbody>
+		</table>
+
+	</div>
+</form>
