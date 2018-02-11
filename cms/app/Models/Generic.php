5ceed379f2af1bee91919b7019c1c392cb135bd8
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-02-11 19:03:20 +0200

    make page number a variable

--- cms/app/Models/Generic.php
+++ cms/app/Models/Generic.php
@@ -9,7 +9,22 @@ class Generic
 	static function fetchPosts()
 	{
 		$db = Connection::connect();
-		$sql = "SELECT * FROM cms.posts WHERE cms.posts.post_status = 'Published' LIMIT 5, 5";
+
+		$per_page = 3;
+		if (isset($_GET['page'])) {
+			$page = $_GET['page'];
+		} else {
+			$page = "";
+		}
+
+		if ($page == "" || $page === 1) {
+			$page_1 = 0;
+		} else {
+			$page_1 = ($page * $per_page) - $per_page;
+			var_dump($page_1);
+		}
+
+		$sql = "SELECT * FROM cms.posts WHERE cms.posts.post_status = 'Published' LIMIT $page_1, $per_page";
 		$query = mysqli_query($db, $sql);
 
 		if ( ! $query) {
@@ -31,8 +46,13 @@ class Generic
 		$sql = "SELECT * FROM cms.posts";
 		$find_count = mysqli_query($db, $sql);
 		$count = mysqli_num_rows($find_count);
-		$count = ceil($count / 6);
+		$count = ceil($count / 5); // make int, not float
 		return $count;
+	}
+
+	static function pageNumberSetter()
+	{
+		// TODO
 	}
 }
 
