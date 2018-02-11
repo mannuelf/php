cacf184815135007bfebe24fdf7adb8bdc659171
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-02-11 16:16:48 +0200

    created a Query to get the count of of posts and for loop to echo out all the links with the numbers in em

--- cms/app/Models/Generic.php
+++ cms/app/Models/Generic.php
@@ -9,7 +9,7 @@ class Generic
 	static function fetchPosts()
 	{
 		$db = Connection::connect();
-		$sql = "SELECT * FROM cms.posts WHERE cms.posts.post_status = 'Published' ";
+		$sql = "SELECT * FROM cms.posts WHERE cms.posts.post_status = 'Published' LIMIT 5, 5";
 		$query = mysqli_query($db, $sql);
 
 		if ( ! $query) {
@@ -22,6 +22,17 @@ class Generic
 		}
 
 		return $result;
+	}
+
+	// return number of posts by counting the number of rows
+	static function fetchPostCount()
+	{
+		$db = Connection::connect();
+		$sql = "SELECT * FROM cms.posts";
+		$find_count = mysqli_query($db, $sql);
+		$count = mysqli_num_rows($find_count);
+		$count = ceil($count / 5);
+		return $count;
 	}
 }
 
