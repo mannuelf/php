3e495ca1c229e853b3195cc4a547f81fcbb2aeda
geektumis <geektumis@gmail.com>
2018-02-27 10:43:37 +0200

    First User Seed

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class UserSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+		$data = [
+			[
+				'username' =>'MannyF',
+				'password' => 'silver09',
+				'password_salt' => '0',
+				'email' => 'mannuel.ferreira@gmail.com',
+				'first_name' => 'Mannuel',
+				'last_name' => 'Ferrreira',
+				'image' => 'zolor-zj_400x400.jpg',
+				'role_id' => 1,
+				'active' => 1,
+				'created_at' => date('Y-m-d H:i:s'),
+				'updated_at' => date('Y-m-d H:i:s'),
+				'deleted_at'       => date('Y-m-d H:i:s'),
+			]
+		];
+
+		$posts = $this->table('users');
+		$posts->insert($data)
+			->save();
+    }
+}
